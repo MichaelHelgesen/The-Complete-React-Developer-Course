e53a9fe70efb25768a1b677537c9b5d29149da1a
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-10-09 23:15:15 +0200

    Continuing course

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,77 +5,80 @@ console.log("App.js is running");
 var app = {
     title: "Title",
     subtitle: "Subtitle",
-    options: ["one", "two"]
+    options: []
 };
 
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options.length ? "Here are your options" : "No options"
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "one"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "two"
-        )
-    )
-);
-
-var user = {
-    name: "Mikke",
-    age: 39,
-    location: "Halden"
-};
-
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            "p",
-            null,
-            "Location: ",
-            location
-        );
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        renderToDom();
     }
-}
+};
 
-var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        user.name ? user.name : "Anonymous"
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        "p",
-        null,
-        "Age: ",
-        user.age
-    ),
-    getLocation(user.location)
-);
+var resetOptions = function resetOptions() {
+    app.options = [];
+    renderToDom();
+};
+
+var numbers = [55, 101, 1000];
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+var renderToDom = function renderToDom() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length ? "Here are your options" : "No options"
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            "button",
+            { onClick: resetOptions },
+            "Remove all"
+        ),
+        React.createElement(
+            "ol",
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    "li",
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            "form",
+            { onSubmit: onFormSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add option"
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderToDom();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,42 +3,48 @@ console.log("App.js is running");
 const app = {
     title: "Title",
     subtitle: "Subtitle",
-    options: ["one", "two"]
-}
-
-
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length ? "Here are your options" : "No options"}</p>
-        <ol>
-            <li>one</li>
-            <li>two</li>
-        </ol>
-    </div>
-);
-
-const user = {
-    name: "Mikke",
-    age: 39,
-    location: "Halden"
+    options: []
 };
 
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        renderToDom();
     }
+};
+
+const resetOptions = () => {
+    app.options = [];
+    renderToDom();
 }
 
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : "Anonymous"}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
+const numbers = [55, 101, 1000];
 
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+const renderToDom = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length ? "Here are your options" : "No options"}</p>
+            <p>{app.options.length}</p>
+            <button onClick={resetOptions}>Remove all</button>
+            <ol>
+                {
+                    app.options.map((option) => <li key={option}>{option}</li>)
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
+
+renderToDom();
