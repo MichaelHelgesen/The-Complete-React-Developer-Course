d49f524ce53ff34bf6801b19a1eff63f66c12c4a
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-10-17 22:55:38 +0200

    Updates, challeges etc

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,17 +4,42 @@ class IndecisionApp extends React.Component {
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOptions = this.handleAddOptions.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
         this.state = {
-            options: props.options
-        }
+            options: []
+        };
     }
     handleDeleteOptions() {
-        this.setState(() => {
-            return {
-                options: []
-            };
-        });
+        this.setState(() => ({options: []}));
     }
+
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem("options");
+            const options = JSON.parse(json);
+            if (options) {
+                this.setState(() => ({ options: options }));
+            }
+        } catch (e) {
+            // Do nothing
+        }
+        
+        
+        
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem("options", json);
+        }
+        
+    }
+
+    handleDeleteOption(optionToRemove) {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        })
+    )}
 
     handlePick() {
        alert(
@@ -31,12 +56,8 @@ class IndecisionApp extends React.Component {
             return "This option already exsits";
         }
         
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat(option)
-            }
-        });
-    }
+        this.setState((prevState) => ({options: prevState.options.concat(option)}))
+    };
 
     render() {
         const subTitle = "Put your life in the hands of a computer";
@@ -50,6 +71,7 @@ class IndecisionApp extends React.Component {
                 <Options 
                     options={this.state.options} 
                     handleDeleteOptions={this.handleDeleteOptions}
+                    handleDeleteOption={this.handleDeleteOption}
                 />
                 <AddOptions
                     handleAddOptions={this.handleAddOptions}
@@ -57,10 +79,6 @@ class IndecisionApp extends React.Component {
             </div>
         )
     }
-}
-
-IndecisionApp.defaultProps = {
-        options: []
 }
 
 const Header = (props) => {
@@ -93,8 +111,15 @@ const Options = (props) => {
         return (
             <div>
                 <button onClick={props.handleDeleteOptions}>Remove all</button>
+                {props.options.length === 0 && <p>Please add an option to get started!</p>}
                 {
-                    props.options.map((option) => <Option key={option} optionText={option} />)
+                    props.options.map((option) => (
+                        <Option 
+                            key={option} 
+                            optionText={option}
+                            handleDeleteOption={props.handleDeleteOption} 
+                        />
+                    ))
                 }
 
             </div>  
@@ -105,6 +130,13 @@ const Option = (props) => {
         return (
             <div>
                 {props.optionText}
+                <button 
+                    onClick={(e) => {
+                        props.handleDeleteOption(props.optionText);
+                    }}
+                >
+                    Remove
+            </button>
             </div>
         )
 };
@@ -122,9 +154,12 @@ class AddOptions extends React.Component {
         const inputValue = e.target.elements.option.value.trim();
         const error = this.props.handleAddOptions(inputValue)
 
-        this.setState(() => {
-            return { error };
-        });
+        this.setState(() => ({ error }));
+
+        if (!error) {
+            e.target.elements.option.value = "";
+        }
+
     }
 
     render() {
--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,10 +5,28 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         };
     };
 
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem("count");
+            const count = parseInt(json, 10);
+            
+            if (!isNaN(count)) {
+                this.setState(() => ({ count }));
+            }
+        } catch (e) {
+            // Do nothing
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem("count", this.state.count);
+        }
+    }
     
     handleAddOne() {
         this.setState((prevState) => {
@@ -46,9 +64,6 @@ class Counter extends React.Component {
     }
 };
 
-Counter.defaultProps = {
-    count: 0
-}
 
 ReactDOM.render(<Counter count={10} />, document.getElementById("app"));
 
