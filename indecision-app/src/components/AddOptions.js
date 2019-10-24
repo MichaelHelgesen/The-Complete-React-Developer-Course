9072d80317c0f2de328fdd1aaf9da87fff5b99dd
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-10-24 22:59:20 +0200

    Updates, styling

--- indecision-app/src/components/AddOptions.js
+++ indecision-app/src/components/AddOptions.js
@@ -21,10 +21,10 @@ export default class AddOptions extends React.Component {
     render() {
         return (
             <div>
-            {this.state.error && <p>{this.state.error}</p>}    
-            <form onSubmit={this.handleAddOptions}>
-                    <input type="text" name="option" />
-                    <button>Add Option</button>
+            {this.state.error && <p className="add-option-error">{this.state.error}</p>}    
+            <form className="add-option" onSubmit={this.handleAddOptions}>
+                    <input className="add-option__input" type="text" name="option" />
+                    <button className="button">Add Option</button>
                 </form>
             </div>
             
--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -80,14 +80,17 @@ export default class IndecisionApp extends React.Component {
                             hasOptions={this.state.options.length > 0}
                             handlePick={this.handlePick}
                         />
-                        <Options 
-                            options={this.state.options} 
-                            handleDeleteOptions={this.handleDeleteOptions}
-                            handleDeleteOption={this.handleDeleteOption}
-                        />
-                        <AddOptions
-                            handleAddOptions={this.handleAddOptions}
-                        />
+                        <div className="widget">
+                            <Options 
+                                options={this.state.options} 
+                                handleDeleteOptions={this.handleDeleteOptions}
+                                handleDeleteOption={this.handleDeleteOption}
+                            />
+                            <AddOptions
+                                handleAddOptions={this.handleAddOptions}
+                            />
+                        </div>
+                        
                 </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
