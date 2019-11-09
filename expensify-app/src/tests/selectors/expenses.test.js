a10af19cbc6c6a0b7030a4348e289b04bc360a23
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-11-09 23:03:15 +0100

    Updates

--- expensify-app/src/tests/selectors/expenses.test.js
+++ expensify-app/src/tests/selectors/expenses.test.js
@@ -43,14 +43,14 @@ test ("should filter by start date", () => {
     expect(result).toEqual( [expenses[0], expenses[2] ])
 });
 
-test ("should filter by end date", () => {
+test ("should filter by endDate", () => {
     const filters = {
         text: "",
         sortBy: "date",
         startDate: undefined,
-        endDate: moment(0)
-    };
+        endDate: moment(0).add(2, "days")
+    }
     const result = selectExpenses(expenses, filters);
-    expect(result).toEqual([ expenses[2], expenses[1] ])
+    expect(result).toEqual([expenses[2], expenses[0], expenses[1]])
 });
 
