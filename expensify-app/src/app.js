10d84d8d686f0dc8bfdb57c71a46760236516cce
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-11-18 22:41:14 +0100

    Updates

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -12,23 +12,13 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense( {description:"Water bill", amount: 435}));
-store.dispatch(addExpense( {description:"gas bill", createdAt: 1000}));
-store.dispatch(addExpense( {description:"Rent", amount: 1456}));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses)
-
 console.log(store.getState())
 
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
-)
+);
 
 ReactDOM.render(jsx, document.getElementById("app"));
 
