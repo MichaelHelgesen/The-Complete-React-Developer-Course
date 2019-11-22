19fd2b5ba733e1fe8c544d17ada678ce15686655
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-11-23 00:08:58 +0100

    Updates

--- expensify-app/src/selectors/expenses-total.js
+++ expensify-app/src/selectors/expenses-total.js
@@ -1,31 +1,3 @@
-
-const expenses = [{
-    id: "1",
-    description: "Gum",
-    note: "",
-    amount: 195,
-    createdAt: 0
-}, {
-    id: "2",
-    description: "Rent",
-    note: "",
-    amount: 109500,
-    createdAt: moment(0).subtract(4, "days").valueOf()
-}, {
-    id: "3",
-    description: "Credit Card",
-    note: "",
-    amount: 4500,
-    createdAt: moment(0).add(4, "days").valueOf()
-}];
-
-getExpensesTotal = (arr) => {
-    let total = 0;
-    arr.map((item) => {
-        total = total + item.amount;
-    });
-    return total;
-};
-
-const total = getExpensesTotal(expenses);
-console.log(total);
+export default () => {
+    
+}
