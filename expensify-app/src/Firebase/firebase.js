3439ef44f13731ebe359766bbdf638675e3fea01
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-11-25 21:26:31 +0100

    Updates

---
+++ expensify-app/src/Firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4UVZIYoXEX_gvKcLBQU1KhAQcQScIaaw",
+    authDomain: "expensify-a0641.firebaseapp.com",
+    databaseURL: "https://expensify-a0641.firebaseio.com",
+    projectId: "expensify-a0641",
+    storageBucket: "expensify-a0641.appspot.com",
+    messagingSenderId: "335277010251",
+    appId: "1:335277010251:web:a9bf7d3052737885f30cce"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().set({
+    name: "Michael Helgesen"
+});
