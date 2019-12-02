d99ff4f153591ac82aba489aa316eecbb02dcb2e
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-12-02 21:44:22 +0100

    test

--- expensify-app/src/playground/promises.js
+++ expensify-app/src/playground/promises.js
@@ -1,15 +1,24 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: "Mikke",
-        //     age: 39
-        // });
+        resolve({
+            name: "Mikke",
+            age: 39
+        });
     reject("Something went wrong");
     }, 1500)
 });
 
 promise.then((data) => {
     console.log("2", data);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("this is my other promise");
+        reject("Something went wrong");
+        }, 1500)
+    });
+}).then((str) => {
+    console.log("does this run?", str)
 }).catch((error) => {
     console.log("error: ", error)
 });
