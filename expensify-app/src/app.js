ddb49ccca01c971b0ea0decbc96ee0a777c9d2f6
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-12-09 22:53:55 +0100

    Updates

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import AppRouter from "./routers/AppRouter";
+import AppRouter, {history} from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
 import { setTextFilter } from "./actions/filter";
@@ -11,10 +11,9 @@ import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 import "./firebase/firebase";
 // import "./playground/promises"
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
-
-console.log(store.getState())
 
 const jsx = (
     <Provider store={store}>
@@ -22,11 +21,30 @@ const jsx = (
     </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById("app"));
+        hasRendered = true;
+    }
+}
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById("app"));
+
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === "/") {
+                history.push("/dashboard")
+            }
+        }); 
+    } else {
+        renderApp();
+        history.push("/");
+        
+    }
 });
-
-
 
