9d0ccb90d914c118e313d92e9bbda675d4159b2c
Michael Helgesen <michaelhelgesen02@gmail.com>
2019-12-12 22:15:31 +0100

    Styling

--- expensify-app/src/components/LoginPage.js
+++ expensify-app/src/components/LoginPage.js
@@ -3,8 +3,13 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+
+    <div className="box-layout__box">
+        <div>
+            <h1>Expensify App</h1>
+            <p className="">It's time to get your expenses under control.</p>
+            <button onClick={startLogin}>Login</button>
+        </div>
     </div>
 );
 
